8e7240223b3be72841771bace1520aa54a5ba7fc
unknown <alirezaethdev@gmail.com>
2026-02-27 22:15:07 +0330

    [MODIFIED] Update `contracts.json` when `restoreContract()` command tried, revised

--- src/services/addContracts.js
+++ src/services/addContracts.js
@@ -40,15 +40,16 @@ export const contracts = getContractJSON();
 export async function restore() {
     if(contracts) {
         console.info(`Stored contracts instantiating ...`);
-        for(let i = 0; i < contracts.length; i++) {
-            await add(
+        const contractsLength = contracts.length;
+        for(let i = 0; i < contractsLength; i++) {
+            await _add(
                 contracts[i].name,
                 contracts[i].address,
                 configFile.defaultWallet.index,
                 contracts[i].abiPath
             );
         }
-        console.info(`All cached contracts instantiated!`);
+        console.info(`All stored contracts instantiated!`);
     } else {
         return `The storage is empty!`;
     }
@@ -203,92 +204,112 @@ export async function deploy(contractName, args, accIndex, chain, abiLoc, byteco
  */
 export async function add(contractName, contractAddr, accIndex, abiLoc, chain) {
     try {
-        let currentProvider;
-        let connectedChain;
-        let wallet;
-
-        if(!contractAddr) {
-            throw new Error('Contract address may not be null or undefined!');
-        }
-
-        if(chain) {
-            currentProvider = new ethers.JsonRpcProvider(chain);
-        } else {
-            currentProvider = provider;
-        }
-
-        if(!accIndex && accIndex !== 0) {
-            accIndex = configFile.defaultWallet.index;
-        }
-
-        wallet = new ethers.Wallet(allAccounts[accIndex].privateKey, currentProvider);
-        connectedChain = await currentProvider.getNetwork();
-
-        const abiPath = abiLoc || localStorage.getItem(`${contractName}_abi`);
-        const abi  = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
-
-        const newContract = new ethers.Contract(contractAddr, abi, wallet);
-
-        // Create contract proxy
-        // Wrap the contract instace with proxy
-        const proxiedContract = createContractProxy(newContract, currentProvider, allAccounts);
-
-        proxiedContract._contractIndex = contracts.length;
-        proxiedContract._contractName = contractName;
-        proxiedContract._contractChain = connectedChain.name;
-        proxiedContract._contractChainId = connectedChain.chainId;
-        proxiedContract._contractDeployType = 'ethershell-deployed';
-        proxiedContract._contractProvider = currentProvider;
-        proxiedContract._contractABIPath = abiPath;
-
-        // Add to REPL context with proxy
-        r.context[contractName] = proxiedContract;
+        const result = await _add(contractName, contractAddr, accIndex, abiLoc, chain);
         
+        // Decorate contract object
         const contracToStore = {
-            index: proxiedContract._contractIndex,
-            name: proxiedContract._contractName,
-            address: proxiedContract.target,
-            chain: proxiedContract._contractChain,
-            chainId: proxiedContract._contractChainId,
-            deployType: proxiedContract._contractDeployType,
-            balance: await proxiedContract.provider.getBalance(proxiedContract.target),
-            abiPath: proxiedContract._contractABIPath
+            index: result.index,
+            name: result.name,
+            address: result.address,
+            chain: result.chain,
+            chainId: result.chainId,
+            deployType: result.deployType,
+            balance: await result.provider.getBalance(result.address),
+            abiPath: abiLoc
         }
+        // Update contracts array
         contracts.push(contracToStore);
 
         // Update contracts.json
         updateContractJSON(contracts);
 
-        // Update deployer contract list
-        const contSpec = {
-            address: newContract.target,
-            deployedOn: connectedChain.name,
-            chainId: connectedChain.chainId
-        }
-
-        allAccounts[accIndex].contracts.push(contSpec);
-
-        // Decorate contract instance with metadata
-        newContract._contractIndex = contracts.length;
-        newContract._contractName = contractName;
-        newContract._contractChain = connectedChain.name;
-        newContract._contractChainId = connectedChain.chainId;
-        newContract._contractDeployType = 'pre-deployed',
-        newContract._contractProvider = currentProvider;
-
-        // Add result
-        const result = {
-            index: newContract._contractIndex,
-            name: newContract._contractName,
-            address: newContract.target,
-            chain: newContract._contractChain,
-            chainId: newContract._contractChainId,
-            deployType: newContract._contractDeployType,
-            provider: newContract._contractProvider
-        }
-
         return result;
     } catch(err) {
         console.error(err);
     }
+}
+
+async function _add(contractName, contractAddr, accIndex, abiLoc, chain) {
+    let currentProvider;
+    let connectedChain;
+    let wallet;
+
+    if(!contractAddr) {
+        throw new Error('Contract address may not be null or undefined!');
+    }
+
+    if(chain) {
+        currentProvider = new ethers.JsonRpcProvider(chain);
+    } else {
+        currentProvider = provider;
+    }
+
+    if(!accIndex && accIndex !== 0) {
+        accIndex = configFile.defaultWallet.index;
+    }
+
+    wallet = new ethers.Wallet(allAccounts[accIndex].privateKey, currentProvider);
+    connectedChain = await currentProvider.getNetwork();
+
+    const abiPath = abiLoc || localStorage.getItem(`${contractName}_abi`);
+    const abi  = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
+
+    const newContract = new ethers.Contract(contractAddr, abi, wallet);
+
+    // Create contract proxy
+    // Wrap the contract instace with proxy
+    const proxiedContract = createContractProxy(newContract, currentProvider, allAccounts);
+
+    proxiedContract._contractIndex = contracts.length;
+    proxiedContract._contractName = contractName;
+    proxiedContract._contractChain = connectedChain.name;
+    proxiedContract._contractChainId = connectedChain.chainId;
+    proxiedContract._contractDeployType = 'ethershell-deployed';
+    proxiedContract._contractProvider = currentProvider;
+    proxiedContract._contractABIPath = abiPath;
+
+    // Add to REPL context with proxy
+    r.context[contractName] = proxiedContract;
+    
+    // const contracToStore = {
+    //     index: proxiedContract._contractIndex,
+    //     name: proxiedContract._contractName,
+    //     address: proxiedContract.target,
+    //     chain: proxiedContract._contractChain,
+    //     chainId: proxiedContract._contractChainId,
+    //     deployType: proxiedContract._contractDeployType,
+    //     balance: await proxiedContract.provider.getBalance(proxiedContract.target),
+    //     abiPath: proxiedContract._contractABIPath
+    // }
+    // contracts.push(contracToStore);
+
+    // Update deployer contract list
+    const contSpec = {
+        address: newContract.target,
+        deployedOn: connectedChain.name,
+        chainId: connectedChain.chainId
+    }
+
+    allAccounts[accIndex].contracts.push(contSpec);
+
+    // Decorate contract instance with metadata
+    newContract._contractIndex = contracts.length + 1;
+    newContract._contractName = contractName;
+    newContract._contractChain = connectedChain.name;
+    newContract._contractChainId = connectedChain.chainId;
+    newContract._contractDeployType = 'pre-deployed',
+    newContract._contractProvider = currentProvider;
+
+    // Add result
+    const result = {
+        index: newContract._contractIndex,
+        name: newContract._contractName,
+        address: newContract.target,
+        chain: newContract._contractChain,
+        chainId: newContract._contractChainId,
+        deployType: newContract._contractDeployType,
+        provider: newContract._contractProvider
+    }
+
+    return result;
 }
